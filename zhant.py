840c6c9a01cdc5d1ee3f9d143a6c873fd52ef104
LabPC <liu246542@163.com>
2018-01-23 21:18:52 +0800

    change dir & add test

---
+++ zhant.py
@@ -0,0 +1,11 @@
+import threading
+
+from util.zhanlive import Zhanlive
+
+zhanqi = Zhanlive()
+
+# threading.active_count()
+
+thd1 = threading.Thread(target = zhanqi.play, args = ([55]), name = 'play1')
+
+thd1.start()
