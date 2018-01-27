eeb6c21503302b6414c0550da230d4075ed350fd
Liu VPS <liu246542@163.com>
2018-01-27 06:31:53 +0000

    simple test

--- zhant.py
+++ zhant.py
@@ -6,6 +6,6 @@ zhanqi = Zhanlive()
 
 # threading.active_count()
 
-thd1 = threading.Thread(target = zhanqi.play, args = ([55]), name = 'play1')
+thd1 = threading.Thread(target = zhanqi.play, args = ([55]), name = 'test1')
 
 thd1.start()
